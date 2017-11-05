271dbdedf4ff9707fa821009e43def2d1b9be223
Luis Loyh <lloyh@wou.edu>
2017-11-05 11:23:09 -0800

    Updated Node class per the Java code specs for this class

--- CS460/HW3/homework3/Node.cs
+++ CS460/HW3/homework3/Node.cs
@@ -7,6 +7,32 @@ using System.Threading.Tasks;
 namespace homework3
 {
     class Node<T>
-    {           
+    {
+        private T data;
+        private Node<T> next;
+
+        public Node()
+        {
+            data = default(T);
+            next = null;
+        }
+        public Node(T item, Node<T> node)
+        {
+            data = item;
+            next = node;
+        }
+
+        //properties
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public Node<T> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
     }
 }
