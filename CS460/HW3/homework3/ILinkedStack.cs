a794fd5a013d4a20f5656217931dd4367b4fec31
Luis Loyh <lloyh@wou.edu>
2017-11-07 12:09:27 -0800

    Restored homework3 files as there were merging conflics with master

---
+++ CS460/HW3/homework3/ILinkedStack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3
+{
+    interface ILinkedStack<T>
+    {
+        //pop
+        T Pop();
+
+        //peek
+        T Peek();
+
+        //push
+        void Push(T item);
+
+        //clear
+        void Clear();
+
+        //is empty
+        bool IsEmpty();
+    }
+}
---
+++ CS460/HW3/homework3/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedStack<double> stack = new LinkedStack<double>();
+            bool playAgain = true;
+            string[] operators = { "+", "-", "/", "*" };
+            while (playAgain)
+            {
+                stack.Clear();
+                //get input from the user
+                string[] input = Console.ReadLine().Split(' ');
+                //exit if q is pressed
+                if (input[0] == "q")
+                {
+                    playAgain = false;
+                }
+                else
+                {
+                    //do the calculation
+                    foreach (string s in input)
+                    {
+                        //check if the input iteration is an operand
+                        if (IsOperand(s))
+                        {
+                            try
+                            {
+                                stack.Push(Convert.ToDouble(s));
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Error: Invalid character entered!!");
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            int location = -1;
+                            for (int i = 0; i < operators.Length; i++)
+                            {
+                                if (operators[i] == s)
+                                {
+                                    location = i;
+                                }
+                            }
+                            if (location == -1)
+                            {
+                                Console.WriteLine("Invalid input detected");
+                                break;
+                            }
+
+                            //valid operator 
+                            double a = double.NegativeInfinity;
+                            double b = double.NegativeInfinity;
+                            //0 == +
+                            if (location == 0)
+                            {
+                                if (stack.IsEmpty() == false)
+                                {
+                                    b = stack.Pop();
+                                }
+                                if (stack.IsEmpty() == false)
+                                {
+                                    a = stack.Pop();
+                                }
+                                if (a == double.NegativeInfinity || b == double.NegativeInfinity)
+                                {
+                                    Console.WriteLine("The stack is currently empty!!!");
+                                    break;
+                                }
+                                stack.Push(a + b);
+                            }
+                            //1 == -
+                            else if (location == 1)
+                            {
+                                if (stack.IsEmpty() == false)
+                                {
+                                    b = stack.Pop();
+                                }
+                                if (stack.IsEmpty() == false)
+                                {
+                                    a = stack.Pop();
+                                }
+                                if (a == double.NegativeInfinity || b == double.NegativeInfinity)
+                                {
+                                    Console.WriteLine("The stack is currently empty!!!");
+                                    break;
+                                }
+                                stack.Push(a - b);
+                            }
+                            //2 == /
+                            else if (location == 2)
+                            {
+                                if (stack.IsEmpty() == false)
+                                {
+                                    b = stack.Pop();
+                                }
+                                if (stack.IsEmpty() == false)
+                                {
+                                    a = stack.Pop();
+                                }
+                                if (a == double.NegativeInfinity || b == double.NegativeInfinity)
+                                {
+                                    Console.WriteLine("The stack is currently empty!!!");
+                                    break;
+                                }
+                                if (b == 0)
+                                {
+                                    Console.WriteLine("Error: Cannot divide by 0!");
+                                }
+                                stack.Push(a / b);
+                            }
+                            //3 == *
+                            else
+                            {
+                                if (stack.IsEmpty() == false)
+                                {
+                                    b = stack.Pop();
+                                }
+                                if (stack.IsEmpty() == false)
+                                {
+                                    a = stack.Pop();
+                                }
+                                if (a == double.NegativeInfinity || b == double.NegativeInfinity)
+                                {
+                                    Console.WriteLine("The stack is currently empty!!!");
+                                    break;
+                                }
+                                stack.Push(a * b);
+                            }
+                        }
+                    }
+                    Console.WriteLine(stack.Pop().ToString());
+
+                }
+
+            }
+
+        }
+        /// <summary>
+        /// Checks if the input is an operand
+        /// </summary>
+        /// <param name="value">string to be checked</param>
+        /// <returns>true if input is operand</returns>
+        static bool IsOperand(string value)
+        {
+            return new Regex(@"^(?:\d*.)?\d+$").Match(value).Success;
+
+        }
+    }
+}
