d472a07bc00083eb077d398de8c2808b5bb7bb88
Luis Loyh <lloyh@wou.edu>
2017-11-06 19:35:49 -0800

    Added page1 and its functionality via the HomeController - converts Celsius to Fahrenheit and vice versa

--- CS460/HW4/Homework4/Controllers/HomeController.cs
+++ CS460/HW4/Homework4/Controllers/HomeController.cs
@@ -26,5 +26,54 @@ namespace Homework4.Controllers
 
             return View();
         }
+        /*
+        public ActionResult Page1()
+        {
+            ViewBag.Message = "Page 1.";
+
+            return View();
+        }
+        */
+        
+
+        [HttpGet]
+        public ActionResult Page1()
+        {
+            string resultString = "";
+            string temperatureString = Request.QueryString["temperature"];
+            if(!double.TryParse(temperatureString, out double temperatureDouble))
+            {
+                resultString = "Error: Temperature must be a number";
+                ViewBag.response = resultString;
+                return View();
+            }
+
+            string unit = Request.QueryString["unit"];
+            double result = 0;            
+
+            if(unit == "F" || unit == "f" )
+            {
+                result = (temperatureDouble - 32) / 1.8;
+                resultString = result + " Celsius";
+            }
+            else if (unit == "C" || unit == "c")
+            {
+                result = (temperatureDouble * 1.8) +32;
+                resultString = result + " Fahrenheit";
+
+            }
+            else
+            {
+                resultString = "Error: for temperature unit, you can only enter F or f for fahrenheit, or C or c for Celsius";
+                ViewBag.response = resultString;
+                return View();
+            }
+
+            ViewBag.response = resultString;            
+            return View();
+        }
+
+
+
     }
 }
