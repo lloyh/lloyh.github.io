13aed17d42d47e7cb0b171b8b1233ff8c0f967a1
Luis Loyh <lloyh@wou.edu>
2017-11-07 20:21:00 -0800

    Added xml comments to function declarations in ILinkedStack

--- CS460/HW3/homework3/ILinkedStack.cs
+++ CS460/HW3/homework3/ILinkedStack.cs
@@ -9,18 +9,37 @@ namespace homework3
     interface ILinkedStack<T>
     {
         //pop
+        /// <summary>
+        /// Templated declaration for Pop function
+        /// </summary>
+        /// <returns>Templated element on top of stack</returns>
         T Pop();
 
         //peek
+        /// <summary>
+        /// Templated declaration for Peek function
+        /// </summary>
+        /// <returns>Value on top of the stack if stack is not empty</returns>
         T Peek();
 
         //push
+        /// <summary>
+        /// Templated declaration for Push function, pushes T item to top of stack
+        /// </summary>
+        /// <param name="item"></param>
         void Push(T item);
 
         //clear
+        /// <summary>
+        /// Clears the stack, turns top node null
+        /// </summary>
         void Clear();
 
         //is empty
+        /// <summary>
+        /// Checks if stack is empty
+        /// </summary>
+        /// <returns>returns true if stack is empty, otherwise returns false</returns>
         bool IsEmpty();
     }
 }
