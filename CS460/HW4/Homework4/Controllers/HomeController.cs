b1fb4ffe6cf6fb505840ee1d9cd6b86a46bedbf0
Luis Loyh <lloyh@wou.edu>
2017-11-09 20:23:53 -0800

    Created Page2 that uses post method to pass data to controller

--- CS460/HW4/Homework4/Controllers/HomeController.cs
+++ CS460/HW4/Homework4/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace Homework4.Controllers
             ViewBag.Message = "This is my contact page.";
 
             return View();
-        }
+        }       
              
 
         [HttpGet]
@@ -74,14 +74,57 @@ namespace Homework4.Controllers
             return View();
         }
 
-        
+        [HttpGet]
         public ActionResult Page2()
         {
-            ViewBag.Message = "Page 2.";
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Page2(FormCollection form)
+        {
+            string resultString = "";
+            string temperatureString = form["temperature"];
+            string unit = form["unit"];
+            double result = 0;
+
+            //When loading page, both inputs should be empty, do not evaluate, just return empty view
+            if (unit == "" || temperatureString == "" || unit == null || temperatureString == null)
+            {
+                ViewBag.response = resultString;
+                return View();
+            }
+            //test if temperature value is a double
+            if (!double.TryParse(temperatureString, out double temperatureDouble))
+            {
+                resultString = "Error: Temperature must be a number";
+                ViewBag.response = resultString;
+                return View();
+            }
+            //temperature is valid, proceed to evaluate unit value
+            //test for Fahrenheit
+            if (unit == "F" || unit == "f")
+            {
+                result = (temperatureDouble - 32) / 1.8;
+                resultString = result + " Celsius";
+            }
+            //test for Celsius
+            else if (unit == "C" || unit == "c")
+            {
+                result = (temperatureDouble * 1.8) + 32;
+                resultString = result + " Fahrenheit";
+            }
+            //unit input is not valid
+            else
+            {
+                resultString = "Error: for temperature unit, you can only enter F or f for fahrenheit, or C or c for Celsius";
+                ViewBag.response = resultString;
+                return View();
+            }
+
+            ViewBag.response = resultString;
             return View();
         }
         
-
     }
 }
