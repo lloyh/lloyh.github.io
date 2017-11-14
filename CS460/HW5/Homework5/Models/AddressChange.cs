8b43bd70409d6ee2cb73202b9eaabca7d7821011
Luis Loyh <lloyh@wou.edu>
2017-11-13 22:49:01 -0800

    Added model class under Models and DAL folder in which I added the db context class

--- CS460/HW5/Homework5/Models/AddressChange.cs
+++ CS460/HW5/Homework5/Models/AddressChange.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,47 @@ namespace Homework5.Models
 {
     public class AddressChange
     {
+        [Required]
+        [Display (Name ="Change ID")]
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "ODL")]
+        public int ODL { get; set; }
+
+
+        [Required]
+        [Display(Name = "Date of Birth")]
+        public DateTime DOB { get; set; }
+
+
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [Display(Name = "New Street Address")]
+        public string NewStreetAddress { get; set; }
+
+        [Required]
+        [Display(Name = "City")]
+        public string NewCity { get; set; }
+     
+        [Required]
+        [Display(Name = "State")]
+        public string NewState { get; set; }
+
+        [Required]
+        [Display(Name = "Zip Code")]
+        public string NewZipCode { get; set; }
+
+        [Required]
+        [Display(Name = "Country")]
+        public string NewCounty { get; set; }
+
+        [Required]
+        [Display(Name = "Date Submitted")]
+        public DateTime DateSubmitted { get; set; }
+
     }
 }
