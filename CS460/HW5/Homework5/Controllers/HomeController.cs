cbde86817aae4e74e12f03851dc9dfa7b0c85550
Luis Loyh <lloyh@wou.edu>
2017-11-14 22:33:13 -0800

    Added database declaration in controller

--- CS460/HW5/Homework5/Controllers/HomeController.cs
+++ CS460/HW5/Homework5/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Homework5.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,9 @@ namespace Homework5.Controllers
 {
     public class HomeController : Controller
     {
+        private AddressChangeContext myDatabase = new AddressChangeContext();
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
