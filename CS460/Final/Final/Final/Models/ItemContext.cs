2fec3e3a03c3418ca1c04d34e216ae1a7b501aab
Luis Loyh <lloyh@wou.edu>
2017-12-05 18:38:55 -0800

    second commit, app ItemsController added with corresponding view

---
+++ CS460/Final/Final/Final/Models/ItemContext.cs
@@ -0,0 +1,38 @@
+namespace Final.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ItemContext : DbContext
+    {
+        public ItemContext()
+            : base("name=ItemContext")
+        {
+        }
+
+        public virtual DbSet<Bid> Bids { get; set; }
+        public virtual DbSet<Buyer> Buyers { get; set; }
+        public virtual DbSet<Item> Items { get; set; }
+        public virtual DbSet<Seller> Sellers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Buyer>()
+                .HasMany(e => e.Bids)
+                .WithRequired(e => e.Buyer)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Item>()
+                .HasMany(e => e.Bids)
+                .WithRequired(e => e.Item)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Seller>()
+                .HasMany(e => e.Items)
+                .WithRequired(e => e.Seller)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
